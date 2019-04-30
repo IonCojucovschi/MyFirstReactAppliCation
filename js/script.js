911a8994e5fee3bd74a3193ba2f7816720ec4017
ICojucovschi <ICojucovschi@WX-OR6186835>
2019-04-30 21:42:49 +0300

    handle clicks on button withreact js

--- js/script.js
+++ js/script.js
@@ -1,85 +1,16 @@
-const DigitalClock = function (props) {
-    return React.createElement(
-        "div",
-        null,
-        props.time
-    );
-};
-
-class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentTime: new Date().toLocaleString()
-        };
-    }
-    /*
-    ////method that is executing until create the component 
-    componentWillMount(){}
-    componentDidMount(){}
-    ///methods thas is executing in moment when component was created 
-    componentWillReceiveProps(){}
-    shouldComponentUpdate(){}
-    componentWillUpdate(){}
-    componentDidUpdate(){}
-    ///method that is executing in moment when component is destroied
-    componentWillUnmount(){}
-    */
-    componentWillMount() {
-        console.log("element will be mount");
-    }
-    componentDidMount() {
-        console.log("component was mount");
-        this.clockLauncer();
+class Button extends React.Component {
+    clockHANDLER() {
+        console.log("button was clicked!!!!");
     }
 
-    componentWillUpdate() {
-        console.log("component was updated");
-    }
-    componentWillUnmount() {
-        clearInterval(this.state.diapazon);
-    }
-
-    clockLauncer() {
-        let diapazon = setInterval(() => {
-            console.log("Time was change !!");
-            this.setState({
-                currentTime: new Date().toLocaleString()
-            });
-        }, 1000);
-        this.setState({ diapazon: diapazon });
-    }
     render() {
         return React.createElement(
-            "div",
-            null,
-            React.createElement(DigitalClock, { time: this.state.currentTime }),
-            React.createElement(
-                "p",
-                null,
-                "Clock will be remover  in  ",
-                this.props.seconds,
-                " seconds .."
-            )
+            "button",
+            { onClick: this.clockHANDLER.bind(this) },
+            "click me!"
         );
     }
+
 }
 
-let seconds = 5;
-let interval = setInterval(() => {
-    if (seconds === 0) {
-        ReactDOM.render(React.createElement(
-            "div",
-            null,
-            React.createElement(
-                "p",
-                null,
-                "clock has been removed"
-            )
-        ), document.getElementById("root"));
-        clearInterval(interval);
-    } else {
-        ReactDOM.render(React.createElement(Clock, { seconds: seconds }), document.getElementById("root"));
-        seconds--;
-    }
-}, 1000);
+ReactDOM.render(React.createElement(Button, null), document.getElementById("root"));
--- jsx/script.jsx
+++ jsx/script.jsx
@@ -1,88 +1,25 @@
-const DigitalClock=function(props){
-    return <div>{props.time}</div>
+class Button extends React.Component{
+clockHANDLER(){
+    console.log("button was clicked!!!!")
 }
 
-
-class Clock extends React.Component{
-    constructor(props){
-       super(props);
-       this.state={
-         currentTime:(new Date()).toLocaleString()  
-       }
-    }
-    /*
-////method that is executing until create the component 
-componentWillMount(){}
-componentDidMount(){}
-
-///methods thas is executing in moment when component was created 
-componentWillReceiveProps(){}
-shouldComponentUpdate(){}
-componentWillUpdate(){}
-componentDidUpdate(){}
-
-///method that is executing in moment when component is destroied
-componentWillUnmount(){}
-
-*/
-componentWillMount()
-{
-    console.log("element will be mount")
-}
-componentDidMount()
-{
-    console.log("component was mount")
-    this.clockLauncer()
-
-}
-
-componentWillUpdate()
-{
-    console.log("component was updated")
-}
-componentWillUnmount()
-{
-    clearInterval(this.state.diapazon)
-}
-
-
-clockLauncer()
-{
-   let diapazon= setInterval(()=>{
-        console.log("Time was change !!");
-        this.setState({
-            currentTime:(new Date()).toLocaleString()  
-        });
-    },1000);
-    this.setState({diapazon:diapazon})
-}
-    render(){
-        return (
-        <div>
-                <DigitalClock time={this.state.currentTime}/>
-                <p>Clock will be remover  in  {this.props.seconds} seconds ..</p>
-        </div>
-            )
-    }
+    render (){
+    return(
+        <button onClick={this.clockHANDLER.bind(this)}>
+            click me!
+        </button>
+    )
 }
 
 
 
 
-let seconds=5;
-let interval=setInterval(()=>{
-    if(seconds===0)
-    {
-        ReactDOM.render(
-            <div><p>clock has been removed</p></div>,document.getElementById("root")
-        )
-        clearInterval(interval)
-    }else{
-        ReactDOM.render(
-            <Clock seconds={seconds}/>,document.getElementById("root")
-        )
-        seconds--
-    }
-},1000);
+}
 
 
+
+
+ReactDOM.render(
+    <Button />,
+    document.getElementById("root")
+)
