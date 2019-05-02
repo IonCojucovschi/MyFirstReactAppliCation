c39cee41c2469913a5e2d68e9ed713e4e9ce68ef
CojucovschiIon <cojucovschi@gmail.com>
2019-05-03 00:05:43 +0300

    another example of forms

--- js/script.js
+++ js/script.js
@@ -1,14 +1,49 @@
 class Content extends React.Component {
 
-    alertOnSunmitting() {
-        alert("You clicked submit button");
+    constructor(props) {
+        super(props);
+        this.handleRadio = this.handleRadio.bind(this);
+        this.state = {
+            radioGroup: {
+                angular: false,
+                react: true,
+                ember: false
+            }
+        };
+        this.helper();
+    }
+
+    helper() {
+        setInterval(() => {
+            console.log(this.state.radioGroup);
+        }, 1000);
+    }
+
+    handleRadio() {
+        let object = {};
+        object[event.target.value] = event.target.checked;
+        this.setState({ radioGroup: object });
     }
 
     render() {
         return React.createElement(
             "form",
-            { onSubmit: this.alertOnSunmitting.bind(this) },
-            React.createElement("input", { type: "submit", value: "Submit Click" })
+            null,
+            React.createElement("input", {
+                type: "radio",
+                value: "angular",
+                checked: this.state.radioGroup.angular,
+                onChange: this.handleRadio }),
+            React.createElement("input", {
+                type: "radio",
+                value: "react",
+                checked: this.state.radioGroup.react,
+                onChange: this.handleRadio }),
+            React.createElement("input", {
+                type: "radio",
+                value: "ember",
+                checked: this.state.radioGroup.ember,
+                onChange: this.handleRadio })
         );
     }
 }
--- jsx/script.jsx
+++ jsx/script.jsx
@@ -1,16 +1,56 @@
 class Content extends React.Component{
     
-    alertOnSunmitting()
-    {
-        alert("You clicked submit button")
-    }
+ constructor(props){
+     super(props);
+     this.handleRadio=this.handleRadio.bind(this)
+     this.state={
+         radioGroup:{
+             angular:false,
+             react:true,
+             ember:false
+         }
+     }
+     this.helper()
+
+ }
+
+helper(){
+    setInterval(()=>{
+        console.log(this.state.radioGroup)
+    },1000)
+}
+
+handleRadio(){
+let object={};
+object[event.target.value]=event.target.checked
+this.setState({radioGroup:object})
+}
+
 
  render(){
      return(
-         <form onSubmit={this.alertOnSunmitting.bind(this)}>
-             <input type="submit" value="Submit Click"/>
-         </form>
-     )
+<form>
+    <input 
+        type="radio"
+        value="angular"
+        checked={this.state.radioGroup.angular}
+        onChange={this.handleRadio}/>
+<input 
+        type="radio"
+        value="react"
+        checked={this.state.radioGroup.react}
+        onChange={this.handleRadio}/>
+<input 
+        type="radio"
+        value="ember"
+        checked={this.state.radioGroup.ember}
+        onChange={this.handleRadio}/>
+
+
+
+</form>
+
+        )
  }
 }
 
