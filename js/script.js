8cc573c3b3dc42884d206f07c6df0fe008082755
CojucovschiIon <cojucovschi@gmail.com>
2019-05-02 23:28:15 +0300

    in this way we create a form with submitting on this

--- js/script.js
+++ js/script.js
@@ -1,44 +1,14 @@
 class Content extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.clockHANDLER = this.clockHANDLER.bind(this);
-        this.state = { counter: 0 };
+    alertOnSunmitting() {
+        alert("You clicked submit button");
     }
-    clockHANDLER() {
-        console.log("button was clicked!!!!");
-        this.setState({ counter: ++this.state.counter });
-    }
-    render() {
-        return React.createElement(
-            "div",
-            null,
-            React.createElement(Button, { handler: this.clockHANDLER }),
-            React.createElement(Text, { counter: this.state.counter })
-        );
-    }
-}
-
-class Button extends React.Component {
 
     render() {
         return React.createElement(
-            "button",
-            { onClick: this.props.handler },
-            "click me!"
-        );
-    }
-}
-
-class Text extends React.Component {
-
-    render() {
-        return React.createElement(
-            "div",
-            null,
-            "DNT click me! you click ",
-            this.props.counter,
-            " times"
+            "form",
+            { onSubmit: this.alertOnSunmitting.bind(this) },
+            React.createElement("input", { type: "submit", value: "Submit Click" })
         );
     }
 }
--- jsx/script.jsx
+++ jsx/script.jsx
@@ -1,41 +1,17 @@
 class Content extends React.Component{
     
-    constructor(props){
-     super(props)
-     this.clockHANDLER=this.clockHANDLER.bind(this)
-     this.state={counter:0}
+    alertOnSunmitting()
+    {
+        alert("You clicked submit button")
     }
-    clockHANDLER(){
-    console.log("button was clicked!!!!")
-    this.setState({counter: ++this.state.counter})
-    }
-    render (){
-        return(
-            <div> 
-                <Button handler={this.clockHANDLER}/>
-                <Text  counter={this.state.counter}/>
-            </div>
-        )}
-}
 
-class Button extends React.Component{
-
-    render (){
-        return(
-            <button onClick={this.props.handler}>
-                click me!
-            </button>
-        )}
-}
-
-class Text extends React.Component{
-
-    render (){
-        return(
-            <div >
-                DNT click me! you click {this.props.counter} times
-            </div>
-        )}
+ render(){
+     return(
+         <form onSubmit={this.alertOnSunmitting.bind(this)}>
+             <input type="submit" value="Submit Click"/>
+         </form>
+     )
+ }
 }
 
 
