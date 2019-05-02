55342d05f27c38a5d3bf3379bbddb64e1ec98dbc
ICojucovschi <ICojucovschi@WX-OR6186835>
2019-05-02 12:44:29 +0300

    divide one big component in acnother conponent that compose one big ,,,,, in this example is an component that contain two components text and button is cool

--- js/script.js
+++ js/script.js
@@ -1,25 +1,46 @@
-class Button extends React.Component {
+class Content extends React.Component {
 
     constructor(props) {
         super(props);
         this.clockHANDLER = this.clockHANDLER.bind(this);
         this.state = { counter: 0 };
     }
-
     clockHANDLER() {
         console.log("button was clicked!!!!");
         this.setState({ counter: ++this.state.counter });
     }
     render() {
         return React.createElement(
+            "div",
+            null,
+            React.createElement(Button, { handler: this.clockHANDLER }),
+            React.createElement(Text, { counter: this.state.counter })
+        );
+    }
+}
+
+class Button extends React.Component {
+
+    render() {
+        return React.createElement(
             "button",
-            { onClick: this.clockHANDLER },
-            "click me! you click ",
-            this.state.counter,
+            { onClick: this.props.handler },
+            "click me!"
+        );
+    }
+}
+
+class Text extends React.Component {
+
+    render() {
+        return React.createElement(
+            "div",
+            null,
+            "DNT click me! you click ",
+            this.props.counter,
             " times"
         );
     }
-
 }
 
-ReactDOM.render(React.createElement(Button, null), document.getElementById("root"));
+ReactDOM.render(React.createElement(Content, null), document.getElementById("root"));
--- jsx/script.jsx
+++ jsx/script.jsx
@@ -1,32 +1,50 @@
-class Button extends React.Component{
+class Content extends React.Component{
     
     constructor(props){
      super(props)
      this.clockHANDLER=this.clockHANDLER.bind(this)
      this.state={counter:0}
     }
-
     clockHANDLER(){
     console.log("button was clicked!!!!")
     this.setState({counter: ++this.state.counter})
-}
+    }
     render (){
-    return(
-        <button onClick={this.clockHANDLER}>
-            click me! you click {this.state.counter} times
-        </button>
-    )
+        return(
+            <div> 
+                <Button handler={this.clockHANDLER}/>
+                <Text  counter={this.state.counter}/>
+            </div>
+        )}
 }
 
+class Button extends React.Component{
 
+    render (){
+        return(
+            <button onClick={this.props.handler}>
+                click me!
+            </button>
+        )}
+}
 
+class Text extends React.Component{
 
+    render (){
+        return(
+            <div >
+                DNT click me! you click {this.props.counter} times
+            </div>
+        )}
 }
 
 
 
 
 ReactDOM.render(
-    <Button />,
+    <Content />,
     document.getElementById("root")
 )
+
+
+
