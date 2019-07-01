d9cdc2abd5356f321571f7e20c0c9a87df7cda6b
ICojucovschi <ICojucovschi@WX-OR6186835>
2019-07-01 14:26:37 +0300

    implement webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: __dirname + '/jsx/app.jsx',
+  output: {
+    path: __dirname + "/js/",
+    filename: 'bundle.js'
+  },
+  devtool: '#sourcemap',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader' },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loaders: ['babel-loader']
+      }
+    ]
+  }
+}
